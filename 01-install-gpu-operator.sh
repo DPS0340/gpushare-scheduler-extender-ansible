8af0a36fc634cf3db2945c7d0be8a9eb42d05d55
Jiho Lee <optional.int@kakao.com>
2024-05-15 18:50:36 +0900

    fix: Use helm upgrade when gpu operator install

--- 01-install-gpu-operator.sh
+++ 01-install-gpu-operator.sh
@@ -6,5 +6,5 @@ PS4="\n\033[1;33m>>\033[0m "; set -x
 LOCATION=$(realpath "$0")
 DIR=$(dirname "$LOCATION")
 
-helm install --wait -n gpu-operator --create-namespace gpu-operator nvidia/gpu-operator
+helm upgrade --install --wait -n gpu-operator --create-namespace gpu-operator nvidia/gpu-operator
 
